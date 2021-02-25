4bdbf203afe218862fb2698d03b2aa1cbc256536
Aurélien Ooms <aurelien.ooms@gmail.com>
2021-02-25 12:03:24 +0100

    :boom: BREAKING CHANGE(api): Do not use default export.

--- src/index.js
+++ src/index.js
@@ -3,12 +3,4 @@ import TOLERANCE from './TOLERANCE.js';
 import clearAlarm from './clearAlarm.js';
 import setAlarm from './setAlarm.js';
 
-/* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
-export default {
-	Alarm,
-	TOLERANCE,
-	clearAlarm,
-	setAlarm,
-};
-
 export {Alarm, TOLERANCE, clearAlarm, setAlarm};

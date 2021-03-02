4762789668790fba913c996a52d107d44d1d483a
Aurélien Ooms <aurelien.ooms@gmail.com>
2021-03-02 16:17:51 +0100

    :books: docs(Alarm): Document class and methods.

--- src/Alarm.js
+++ src/Alarm.js
@@ -1,8 +1,21 @@
+/**
+ * The type returned by setAlarm.
+ */
 export default class Alarm {
+	/**
+	 * Construct an Alarm given a setTimeout handle.
+	 *
+	 * @param {any} handle
+	 */
 	constructor(handle) {
 		this._handle = handle;
 	}
 
+	/**
+	 * Record a new setTimeout handle (and clear previous one if any).
+	 *
+	 * @param {any} handle
+	 */
 	record(handle) {
 		// NB: This works even if this._handle was created with setInterval.
 		// See: https://html.spec.whatwg.org/multipage/timers-and-user-prompts.html#dom-setinterval
@@ -10,6 +23,9 @@ export default class Alarm {
 		this._handle = handle;
 	}
 
+	/**
+	 * Clear last setTimeout handle.
+	 */
 	clear() {
 		this.record(undefined);
 	}
